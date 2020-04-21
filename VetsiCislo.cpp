900d6881a3fa968d550c867232403c227a3f373f
janecma@student.uzlabina.cz <janecma@student.uzlabina.cz>
2020-04-21 23:57:58 +0200

    Initial commit

---
+++ VetsiCislo.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+ void MinCislo(int x, int y)
+ {
+	
+	if (x < y)
+	{
+		printf("mensi cislo je: %i", x);
+	}
+	else
+	{
+		printf("mensi cislo je: %i", y);
+	}
+ }
+
+ void main()
+ {
+	 int x, y;
+	 scanf_s("%i", &x);
+	 scanf_s("%i", &y);
+	 MinCislo(x,y);
+
+ }
